3a672d28c8b3bd55bf230718e1edaebfacadbe10
ryanowino <owinoryan@gmail.com>
2024-07-04 00:29:40 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+
+// index.js
+
+// Function declaration for saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  module.exports = {
+    saturdayFun
+  };
+
+
+ function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+  };
+  
+  console.log(mondayWork()); 
+  //"You are *a hard worker*!"
+function wrapAdjective(highlight = '*') {
+  let adjective ='a hard worker';
+  return function(adjective) {
+    return `You are ${highlight}${adjective}${highlight}!`;
+  }
+}
